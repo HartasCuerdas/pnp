70026cc9207d52fbc84a32cd6100eb91914b8724
Franco Cedillo <franco.cedillo@gmail.com>
2014-08-19 12:25:39 -0500

    adds JQuery Effect and highlight function

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,24 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require jquery-ui/effect-highlight
 //= require turbolinks
 //= require_tree .
+
+jQuery(function($) {
+
+  $(".btn-enable").on("ajax:success", function(xhr, data, status) {
+      //console.log('ajax success');
+      $(this).toggleClass('btn-success');
+      $(this).toggleClass('btn-default');
+      //console.log($(this).text());
+      if ( $(this).text() == 'ON') {
+      	$(this).html('OFF');
+      } else {
+      	$(this).html('ON');
+      };
+      //$(this).closest('tr').fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+      $(this).closest('tr').effect("highlight", {}, 1500);
+  });
+
+});
