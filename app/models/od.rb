a1152e65bc7e293e63085f9921aefe5d1e93436c
Franco Cedillo <franco.cedillo@gmail.com>
2014-08-19 00:44:57 -0500

    time key from instant datetime

--- app/models/od.rb
+++ app/models/od.rb
@@ -1,2 +1,8 @@
 class Od < ActiveRecord::Base
+	before_create :defaulttimekey
+	private
+	  def defaulttimekey
+      strtime = self.instant.strftime('%H%M')
+      self.timekey = strtime
+    end
 end
