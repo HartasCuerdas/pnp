969760dc9e3a314e9e3cb464f43263173a417ef6
Franco Cedillo <franco.cedillo@gmail.com>
2014-08-19 21:41:39 -0500

    timekey value based on instant field value

--- app/models/od.rb
+++ app/models/od.rb
@@ -1,8 +1,16 @@
 class Od < ActiveRecord::Base
-	before_create :defaulttimekey
+	belongs_to :day
+	#attr_accessible :timekey, :day_id
+	#before_create :defaulttimekey
 	private
 	  def defaulttimekey
-      strtime = self.instant.strftime('%H%M')
+	  	strmin = self.instant.strftime('%M')
+	  	if (strmin.to_i >= 30)
+	  		strmin = '30'
+	  	else
+	  		strmin = '00'
+	  	end
+      strtime = self.instant.strftime('%H:') + strmin
       self.timekey = strtime
-    end
+  end
 end
