98982faf91c5670923a33a0401ddfa7b5cdb4731
Franco Cedillo <franco.cedillo@gmail.com>
2014-08-22 19:48:22 -0500

    removes jquery code

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,21 +15,3 @@
 //= require jquery-ui/effect-highlight
 //= require turbolinks
 //= require_tree .
-
-jQuery(function($) {
-
-  $(".btn-enable").on("ajax:success", function(xhr, data, status) {
-      //console.log('ajax success');
-      $(this).toggleClass('btn-success');
-      $(this).toggleClass('btn-default');
-      //console.log($(this).text());
-      if ( $(this).text() == 'ON') {
-      	$(this).html('OFF');
-      } else {
-      	$(this).html('ON');
-      };
-      //$(this).closest('tr').fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
-      $(this).closest('tr').effect("highlight", {}, 1500);
-  });
-
-});
