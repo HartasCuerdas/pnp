ed3872cf37dd62ee75d29bad3badca2fb9c83ddc
Franco Cedillo <franco.cedillo@gmail.com>
2014-08-22 19:45:57 -0500

    fixes indentation

--- app/helpers/ods_helper.rb
+++ app/helpers/ods_helper.rb
@@ -1,6 +1,6 @@
 module OdsHelper
-	def enable_link_text(o)  
-	  o ? 'ON' : 'OFF'  
+	def enable_link_text(o)
+		o ? 'ON' : 'OFF'
 	end
 	def enable_btn_class(o)
 		o ? 'btn-success' : 'btn-default'
