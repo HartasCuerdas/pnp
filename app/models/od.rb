2c6e843e85d10582193b537265642ff283e595bf
Franco Cedillo <franco.cedillo@gmail.com>
2014-08-27 16:33:46 -0500

    clears Od class

--- app/models/od.rb
+++ app/models/od.rb
@@ -1,16 +1,3 @@
 class Od < ActiveRecord::Base
 	belongs_to :day
-	#attr_accessible :timekey, :day_id
-	#before_create :defaulttimekey
-	private
-	  def defaulttimekey
-	  	strmin = self.instant.strftime('%M')
-	  	if (strmin.to_i >= 30)
-	  		strmin = '30'
-	  	else
-	  		strmin = '00'
-	  	end
-      strtime = self.instant.strftime('%H:') + strmin
-      self.timekey = strtime
-  end
 end
