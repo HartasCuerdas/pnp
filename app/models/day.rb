1b9fc32ee82b60d3d9f41456d6b2769585f00c18
Franco Cedillo <franco.cedillo@gmail.com>
2014-08-27 16:11:31 -0500

    includes constants, refactored methods

--- app/models/day.rb
+++ app/models/day.rb
@@ -1,4 +1,24 @@
 class Day < ActiveRecord::Base
 	#attr_accessible :date
 	has_many :ods
+
+	WELL_REGISTERED_TEXT_TRUE = 'Well'
+	WELL_REGISTERED_TEXT_FALSE = 'Poor'
+	
+	# Twitter Bootstrap Button Style Class
+	WELL_REGISTERED_STYLE_TRUE = 'btn-success'
+	WELL_REGISTERED_STYLE_FALSE = 'btn-danger'
+
+	def str_date
+		self.date.strftime('%b %e, %a')
+	end
+
+	def str_wellRegistered
+		self.well_registered ? WELL_REGISTERED_TEXT_TRUE : WELL_REGISTERED_TEXT_FALSE
+	end
+
+	def str_wellRegistered_TwbsBtnStyleClass
+		self.well_registered ? WELL_REGISTERED_STYLE_TRUE : WELL_REGISTERED_STYLE_FALSE
+	end
+
 end
