8f6907fced63dbf94af178f5edde63f06587316f
Franco Cedillo <franco.cedillo@gmail.com>
2014-08-29 11:00:41 -0500

    Day#before_create :create_ods
    & dependent: :destroy

--- app/models/day.rb
+++ app/models/day.rb
@@ -1,7 +1,8 @@
 class Day < ActiveRecord::Base
 	#attr_accessible :date
-	has_many :ods
+	has_many :ods, dependent: :destroy
 	belongs_to :days
+	before_create :create_ods
 
 	WELL_REGISTERED_TEXT_TRUE = 'Well'
 	WELL_REGISTERED_TEXT_FALSE = 'Poor'
@@ -27,5 +28,36 @@ class Day < ActiveRecord::Base
 	def str_isTodayClass
 		(self.date == Date.today) ? IS_TODAY_STYLE : ''
 	end
+
+	private
+
+		def create_ods
+
+			hoursarr = [
+				'06:00', '06:30',
+				'07:00', '07:30',
+				'08:00', '08:30',
+				'09:00', '09:30',
+				'10:00', '10:30',
+				'11:00', '11:30',
+				'12:00', '12:30',
+				'13:00', '13:30',
+				'14:00', '14:30',
+				'15:00', '15:30',
+				'16:00', '16:30',
+				'17:00', '17:30',
+				'18:00', '18:30',
+				'19:00', '19:30',
+				'20:00', '20:30',
+				'21:00', '21:30',
+				'22:00', '22:30',
+				'23:00', '23:30',
+	    	]
+
+    		hoursarr.each do |hour|
+      			self.ods.new(:timekey => hour)
+    		end
+
+		end
 
 end
