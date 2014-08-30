1f6145be042ce2961b12d3c231666ba9c209e1a0
Franco Cedillo <franco.cedillo@gmail.com>
2014-08-29 19:41:44 -0500

    Sticks top-section on top, for Days. closes #2

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,7 @@ module ApplicationHelper
 	def title(page_title)
 			content_for(:title) { page_title }
 	end
+	def body_classes(page_body_classes)
+			content_for(:body_classes) { page_body_classes }
+	end
 end
