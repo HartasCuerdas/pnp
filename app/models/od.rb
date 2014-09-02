9bc6bf5694c8f8f01e100cda411bc04c14dbb358
Franco Cedillo <franco.cedillo@gmail.com>
2014-09-02 08:39:27 -0500

    (style) tabs to spaces

--- app/models/od.rb
+++ app/models/od.rb
@@ -1,35 +1,35 @@
 class Od < ActiveRecord::Base
-	belongs_to :day
+  belongs_to :day
 
-	OD_TEXT_TRUE = 'ON'
-	OD_TEXT_FALSE = 'OFF'
-	
-	# Twitter Bootstrap Button Style Class
-	OD_STYLE_TRUE = 'btn-success'
-	OD_STYLE_FALSE = 'btn-default'
+  OD_TEXT_TRUE = 'ON'
+  OD_TEXT_FALSE = 'OFF'
+  
+  # Twitter Bootstrap Button Style Class
+  OD_STYLE_TRUE = 'btn-success'
+  OD_STYLE_FALSE = 'btn-default'
 
-	def str_o_enabled
-		str_od_enabled(self.o)
-	end
+  def str_o_enabled
+    str_od_enabled(self.o)
+  end
 
-	def str_d_enabled
-		str_od_enabled(self.d)
-	end
+  def str_d_enabled
+    str_od_enabled(self.d)
+  end
 
-	def str_od_enabled(od)
-		od ? OD_TEXT_TRUE : OD_TEXT_FALSE
-	end
+  def str_od_enabled(od)
+    od ? OD_TEXT_TRUE : OD_TEXT_FALSE
+  end
 
-	def str_o_TwbsBtnStyleClass
-		str_od_TwbsBtnStyleClass(self.o)
-	end
+  def str_o_TwbsBtnStyleClass
+    str_od_TwbsBtnStyleClass(self.o)
+  end
 
-	def str_d_TwbsBtnStyleClass
-		str_od_TwbsBtnStyleClass(self.d)
-	end
+  def str_d_TwbsBtnStyleClass
+    str_od_TwbsBtnStyleClass(self.d)
+  end
 
-	def str_od_TwbsBtnStyleClass(od)
-		od ? OD_STYLE_TRUE : OD_STYLE_FALSE
-	end
+  def str_od_TwbsBtnStyleClass(od)
+    od ? OD_STYLE_TRUE : OD_STYLE_FALSE
+  end
 
 end
