1c617a1829444bba7d79b62c105d7e747f0bf3dc
Franco Cedillo <franco.cedillo@gmail.com>
2014-09-29 17:53:36 -0500

    removes index action

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,10 +1,5 @@
 # Welcome Controller
 class WelcomeController < ApplicationController
-  #index
-  def index
-  	dateToday = Date.today
-    @strToday = dateToday.strftime('%b %d, %a')
-  end
 
   # OPTIONS
   # renders nothing
