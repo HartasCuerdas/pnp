dfce691e03fc2a51ad1a536c19535fc42edbb44a
Franco Cedillo <franco.cedillo@gmail.com>
2014-09-30 11:43:01 -0500

    adds codeclimate support

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,8 +1,19 @@
+require 'codeclimate-test-reporter'
+require 'simplecov'
+require 'coveralls'
+ 
+formatters = [SimpleCov::Formatter::HTMLFormatter]
+ 
+formatters << Coveralls::SimpleCov::Formatter if ENV['COVERALLS_REPO_TOKEN']
+formatters << CodeClimate::TestReporter::Formatter if ENV['CODECLIMATE_REPO_TOKEN']
+ 
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[*formatters]
+ 
+SimpleCov.start 'rails'
+
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
-require 'coveralls'
-Coveralls.wear!
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
