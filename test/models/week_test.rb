ed5ba49193bd5ce6e1ce0f96a21bd500f29bed41
Franco Cedillo <franco.cedillo@gmail.com>
2014-09-30 11:42:39 -0500

    adds test to week model

--- test/models/week_test.rb
+++ test/models/week_test.rb
@@ -4,4 +4,10 @@ class WeekTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "should save week without firstDay" do
+    week = Week.new
+    assert week.save, "Saved the week without a firstDay"
+  end
+
 end
